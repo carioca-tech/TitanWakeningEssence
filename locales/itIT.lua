0fe1b920279e892bfc2bfa8a2fd27fe847952949
Luiz Tavares <luiz-tavares@users.noreply.github.com>
2018-01-22 03:41:36 -0200

    Torturing I18n until it works.

--- locales/itIT.lua
+++ locales/itIT.lua
@@ -1,3 +1,3 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("TitanWakeningEssence", "itIT")
 if not L then return end
---@localization(locale="deDE", format="lua_additive_table", handle-subnamespaces="concat")@
+--@localization(locale="deDE", format="lua_additive_table", handle-subnamespaces="concat", handle-unlocalized="english")@
